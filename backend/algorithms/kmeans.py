6165853b8e163af2efc5681c2e384088a3184187
NocheKnight <62395719+NocheKnight@users.noreply.github.com>
2025-03-30 14:06:39 +0700

    .

--- backend/algorithms/kmeans.py
+++ backend/algorithms/kmeans.py
@@ -1,18 +1,7 @@
 import numpy as np
 from sklearn.cluster import KMeans
-
+# TODO: All
 class KMeansAlgorithm:
     def __init__(self, n_clusters=3):
-        self.model = KMeans(n_clusters=n_clusters, random_state=42)
+        pass
         
-    def fit(self, data):
-        """
-        Обучение модели на данных
-        """
-        return self.model.fit(data)
-    
-    def predict(self, data):
-        """
-        Предсказание кластеров для новых данных
-        """
-        return self.model.predict(data) 
