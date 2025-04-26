18fb2424f4de9ab11f67d7f00562cadbb7e438c5
akortysh <akortysh@yandex.ru>
2025-04-26 07:18:24 +0700

    Hot fix

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException, Body
+from fastapi import FastAPI, File, UploadFile, HTTPException, Body, Form
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
